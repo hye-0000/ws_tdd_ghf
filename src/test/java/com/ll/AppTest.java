226125f4bfc8af3703655b1abc799f191c4d21a1
hye-0000 <hye_0000@naver.com>
2023-03-06 15:14:39 +0900

    출력을 모니í테스트 ã케이스,하지 않고 문자열로 얻기

--- src/test/java/com/ll/AppTest.java
+++ src/test/java/com/ll/AppTest.java
@@ -3,9 +3,11 @@ package com.ll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
@@ -14,5 +16,19 @@ public class AppTest {
         Scanner sc = TestUtil.genScanner("안녕");
         String cmd = sc.nextLine().trim();
         assertThat(cmd).isEqualTo("안녕");
+    }
+
+    @Test
+    @DisplayName("출력을 모니터에 하지 않고 문자열로 얻기")
+    public void t2() {
+        //System.out에 대한 화면 출력 금지
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        System.out.print("안녕");
+        //출력되지 않던 내용들을 문자열로 반환
+        String rs = output.toString();
+        //System.out에 대한 화면출력 금지 끝
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertEquals("안녕", rs);
     }
 }
