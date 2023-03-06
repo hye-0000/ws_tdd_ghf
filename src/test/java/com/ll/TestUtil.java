3576a75870630bba2115538398779b93c310bd4d
hye-0000 <hye_0000@naver.com>
2023-03-06 15:14:39 +0900

    출력을 모니í테스트 ã케이스,하지 않고 문자열로 얻기

--- src/test/java/com/ll/TestUtil.java
+++ src/test/java/com/ll/TestUtil.java
@@ -1,7 +1,6 @@
 package com.ll;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class TestUtil {
@@ -11,5 +10,22 @@ public class TestUtil {
 
         return new Scanner(in);
     }
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream output) {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
 
